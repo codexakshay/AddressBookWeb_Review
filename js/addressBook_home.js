6e8a861b0812767c9e0ce139139dc130cd8335e0
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-12-19 01:56:04 +0530

    Added : Ability To Remove Record From Address Book List

---
+++ js/addressBook_home.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    addressBookList = getAddressBookDataFromStorage();
+    document.querySelector(".person-count").textContent = addressBookList.length;
+    createInnerHtml();
+});
+
+const getAddressBookDataFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ? JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
+
+const createInnerHtml = () => {
+    const headerHtml = "<tr><th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th></th></tr>"
+    if (addressBookList.length == 0) return;
+    let innerHtml = `${headerHtml}`
+    for (const person of addressBookList) {
+        innerHtml = `${innerHtml}
+    <tr>
+    <td>${person._name}</td>
+    <td>${person._address}</td>
+    <td>${person._city}</td>
+    <td>${person._state}</td>
+    <td>${person._zip}</td>
+    <td>${person._phoneNumber}</td>
+    <td>
+        <img id="${person._name}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+        <img id="${person._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+    </td>
+ </tr>
+    `;
+    }
+    document.querySelector("#display").innerHTML = innerHtml;
+};
+
+const remove = (node) => {
+    let personData = addressBookList.find(perData => perData._name == node.id);
+    if (!personData) return;
+    const index = addressBookList.map(perData => perData._name).indexOf(personData._name);
+    addressBookList.splice(index, 1);
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+    document.querySelector(".person-count").textContent = addressBookList.length;
+    createInnerHtml();
+}
